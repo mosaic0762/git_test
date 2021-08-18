1ec1e5226bdbef913c0b78abbc65077add5bb32f
“mosaic0762” <571522671@qq.com>
2021-08-18 23:45:55 +0800

    首次提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author mosaic
+ * @date 2021/8/18-23:35
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
